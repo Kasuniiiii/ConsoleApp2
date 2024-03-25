9ba1b32a01b13b3ed2619130ff76e95dbce216ba
Kasuniiiii <kasunibandara00@gmail.com>
2024-03-25 10:39:39 +0530

    commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,11 +5,17 @@
         static void Main(string[] args)
         {
             
+            int[] numbers={10,20, 30,40,50,25,35,45,78,45};
+            double total = 0;
+            double average = 0;
+           for (int i = 0; i < numbers.Length; i++)
+            {
+            total += numbers[i];
             
-            
-            
-            
-            Console.WriteLine("Hello, World!");
+            }
+           average = total / numbers.Length;
+            Console.WriteLine("total" + total);
+            Console.WriteLine("average" + average);
         }
     }
 }
